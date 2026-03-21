c892b6fc5acead0931d0f30c4ee2cdb4413732fa
hamsaal <hs8403340@gmail.com>
2026-03-21 15:50:47 +0100

    feat(backend): add route and update swagger docs for game detail endpoint

--- backend/src/modules/games/games.routes.ts
+++ backend/src/modules/games/games.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express'
-import { getGamesHandler } from './games.controller'
+import { getGamesHandler, getGameDetailHandler } from './games.controller'
 
 const router = Router()
 
 router.get('/', getGamesHandler)
-
+router.get('/:id', getGameDetailHandler)
 export default router
