82b8fe1cf2725402759ec9166aaf091e08c5a9ac
hamsaal <hs8403340@gmail.com>
2026-03-22 14:15:47 +0100

    feat(frontend): add game detail types

--- frontend/src/pages/games/games.types.ts
+++ frontend/src/pages/games/games.types.ts
@@ -7,3 +7,15 @@ export interface GamesListResponse {
   data: Game[]
   total: number
 }
+export interface Player {
+  id: string
+  name: string
+  avg_score: number
+  miss_count: number
+}
+
+export interface GameDetail {
+  id: number
+  type: string
+  players: Player[]
+}
